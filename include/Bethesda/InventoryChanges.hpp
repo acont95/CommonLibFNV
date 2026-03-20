146a7e1eb902b9e3bc87bbd80989ec97cd2dcdf9
Alex Conticello <alexconticello@gmail.com>
2026-03-20 13:09:11 -0400

    Add InventoryChanges class.

---
+++ include/Bethesda/InventoryChanges.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "Bethesda/Actor.hpp"
+
+namespace CommonLib {
+
+  template <typename T>
+  class BSSimpleList;
+  class ItemChange;
+
+  class InventoryChanges
+  {
+    public:
+      InventoryChanges();
+      ~InventoryChanges();
+
+      BSSimpleList<ItemChange *> *pListofChanges;
+      TESObjectREFR *pRef;
+      float fcontainerweight;
+      float fpreviousContainerWeight;
+      bool bcountdirty;
+  };
+
+  static_assert(sizeof(InventoryChanges) == 0x14, "InventoryChanges has wrong size");
+  
+} // namespace CommonLib
